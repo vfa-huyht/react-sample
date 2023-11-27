e56293a7f02ff70bee24a8005a8ec464d6323156
vfa-huyht <vfa.huyht@gmail.com>
2023-11-27 07:59:40 +0700

    Table

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { useTranslation } from 'react-i18next';
+import Gantt from '../components/Gantt';
 
 export const Home = () => {
   const { t } = useTranslation();
   return (
     <>
-      <h1>{t('welcome')}</h1>
+      <Gantt></Gantt>
     </>
   )
 }
